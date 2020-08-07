634b616dddbe5496e5b928a6ced6b34a5e3209ae
“Marc <marc_piantone@live.fr>
2020-08-07 16:10:14 -0400

    Added a storeItems const to "shop"

--- client/src/components/Shop.js
+++ client/src/components/Shop.js
@@ -1,5 +1,10 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { getItems } from "./reducers/item.reducer";
 
-const Shop = () => {};
+const Shop = () => {
+  const storeItems = useSelector(getItems);
+  console.log(storeItems);
+};
 
 export default Shop;
