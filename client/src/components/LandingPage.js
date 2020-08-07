6845f2cb181d715060088dd574b0c6ff3dfc3a57
“Marc <marc_piantone@live.fr>
2020-08-07 16:31:55 -0400

    Adjustments

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -3,6 +3,8 @@ import { useSelector } from "react-redux";
 import { getItems } from "./reducers/item.reducer";
 
 const LandingPage = () => {
+  const storeItems = useSelector(getItems);
+  console.log(storeItems);
   return <div>Landing Page</div>;
 };
 
