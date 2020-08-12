3b26665e1ae0fba699afe721af34b52798d04715
“Marc <marc_piantone@live.fr>
2020-08-12 10:30:05 -0400

    Remove handleUpdate.js

--- client/src/components/handleUpdateNumInStock.js
+++
@@ -1,8 +0,0 @@
-const handleUpdateNumInStock = (cartItemArray) => {
-  console.log(cartItemArray);
-  fetch(`/items`, {
-    method: "PUT",
-    body: JSON.stringify({ cartItemArray }),
-    headers: { Accept: "application/json", "Content-Type": "application/json" },
-  }).then((res) => console.log(res));
-};
