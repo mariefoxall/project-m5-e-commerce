8566aa5d058b932e08d683b2acf625ae144cf346
“Marc <marc_piantone@live.fr>
2020-08-13 16:09:53 -0400

    Adjustments

--- client/src/components/Shop.js
+++ client/src/components/Shop.js
@@ -162,22 +162,6 @@ const Shop = () => {
                 <option value="Waist">Waist</option>
                 <option value="Wrist">Wrist</option>
               </Dropdown>
-              <Dropdown
-                onChange={(ev) => toggleCompany(ev)}
-                defaultValue={activeCompany}
-                id="company"
-                name="company"
-              >
-                <option value="All">Show All</option>
-                <option value="1962">Berska</option>
-                <option value="Chest">Chest</option>
-                <option value="Feet">Feet</option>
-                <option value="Hands">Hands</option>
-                <option value="Head">Head</option>
-                <option value="Neck">Neck</option>
-                <option value="Waist">Waist</option>
-                <option value="Wrist">Wrist</option>
-              </Dropdown>
             </BodyLocation>
           </FilterDiv>
         </SpacerDiv>
